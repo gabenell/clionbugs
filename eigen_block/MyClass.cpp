0f5fcc180228ad6c08bd381c3afb8de2bb968674
Gabe Nell <gabe@world-vu.net>
2015-05-08 14:42:14 -0700

    Typedef expansion bug

---
+++ eigen_block/MyClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gabe on 5/8/15.
+//
+
+#include "MyClass.h"
---
+++ eigen_block/MyClass.h
@@ -0,0 +1,18 @@
+//
+// Created by gabe on 5/8/15.
+//
+
+#ifndef EIGEN_BLOCK_MYCLASS_H
+#define EIGEN_BLOCK_MYCLASS_H
+
+#include <Eigen/Geometry>
+
+class MyClass {
+public:
+
+    // On below line, ALT+ENTER and select "implement function 'myMethod'"
+    Eigen::Matrix3d myMethod();
+};
+
+
+#endif //EIGEN_BLOCK_MYCLASS_H
